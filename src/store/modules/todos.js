b3afeeabf3c23db7e6ba6b30d73dba156ae16fa1
Jayakrishnan Damodaran <jayakrishnandamodaran@gmail.com>
2020-04-10 16:24:57 +0530

    Revert limit in fetch todos.

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -12,7 +12,7 @@ const getters = {
 
 const actions = {
     async fetchTodos({ commit }) {
-        const response = await axios.get(BASE_URL + '?_limit=5');
+        const response = await axios.get(BASE_URL);
         commit('setTodos', response.data);
     },
     async addTodo({ commit }, title) {
